90b4293ec596bf4ce38cd51948bdf3b9e8a80817
Alex Robinson <alyr1481@gmail.com>
2017-12-21 21:59:12 +0000

    Weight validation complete and working!

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,19 @@
 
-//function findselected() {
-    var result = $("#weight").value;
-    console.log(result);
-    if(result > 100){
-        document.getElementById("ItemClassDropdown").options[1].disabled = true;
-    }
-//}
+// Check to see if weight is over limit for letter class
+$("#weight").keyup( function () {
+   if (parseInt(this.value) > 100) {
+     $("option[value='letter']").prop('disabled', true);
+     destroyReBuild();
+   } else{
+     $("option[value='letter']").prop('disabled', false);
+     destroyReBuild();
+   }
+});
+
+// Destroy and Rebuild Select Input
+function destroyReBuild(){
+  $('.mdb-select').material_select('destroy');
+  $(document).ready(function() {
+     $('.mdb-select').material_select();
+   });
+}
